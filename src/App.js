e35d3c5497b204dfb48cd6d0fa2d50d47161b596
vinod c s <csvinod844@gmail.com>
2024-10-14 17:57:42 +0530

    content commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Creat DashBoard
+          
         </a>
       </header>
     </div>
